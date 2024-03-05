2453771757c12c6a68e111fc260a6f2126000dc3
Harry MacCode <emarh.harry.code@gmail.com>
2024-03-06 00:30:57 +0100

    Register Route created and connexion to mongodb

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
+dotenv.config();
+
+app.use(express.json());
 app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI);
+// console.log(process.env.MONGO_URI);
 
 app.get('/test', (req, res) => {
     // res.json('test okay');
     console.log('object');
     res.json('test okay');
+});
+
+app.post('/register', (req, res) => {
+    const { name, email, password } = req.body;
+    res.json({ name, email, password });
 });
 
 const PORT = 4080;
