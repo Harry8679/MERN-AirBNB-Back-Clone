ad803336b9dc583d964f81b74a272a45eca92f33
Harry MacCode <emarh.harry.code@gmail.com>
2024-03-06 00:55:10 +0100

    User Model created, Registration of a User

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const User = require('./models/user.model');
+const bcrypt = require('bcryptjs');
+
+const bcryptSalt = bcrypt.genSaltSync(10);
 
 dotenv.config();
 
@@ -18,9 +22,12 @@ app.get('/test', (req, res) => {
     res.json('test okay');
 });
 
-app.post('/register', (req, res) => {
+app.post('/register', async (req, res) => {
     const { name, email, password } = req.body;
-    res.json({ name, email, password });
+    // res.json({ name, email, password });
+    const userDoc = await User.create({ name, email, password: bcrypt.hashSync(password, bcryptSalt) });
+
+    res.json(userDoc);
 });
 
 const PORT = 4080;
